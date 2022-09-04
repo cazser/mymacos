efa90b33c314ff811af2cf0da261c0bb10b1403d
cazser <2264712072@qq.com>
2022-09-04 12:58:35 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import Dock from './components/Dock.vue'
+import { ref } from 'vue';
+
+const isLoading = ref(true);
+setTimeout(()=>{
+  isLoading.value = !isLoading.value
+}, 5000)
+</script>
+
+<template>
+<div v-if="isLoading" class="loading">
+
+</div>
+<div v-if="!isLoading" class="desktop">
+  
+  <Dock></Dock>
+</div> 
+</template>
+
+<style scoped>
+
+.loading{
+  min-height: 100vh;
+  background: white;
+}
+  .desktop{
+    min-height: 100vh;
+    position: relative;
+    background-image: url('./assets/wellpapers/3-1.jpg');
+  }
+
+</style>
---
+++ src/components/Dock.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+    <ol class="dock">
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ol>
+</template>
+
+<style scoped>
+
+.dock{
+  border: 10px solid;
+  color: aqua;
+  display: flex;
+  justify-content: space-between;
+  position: absolute;
+  bottom: 0;
+  left:50%;
+  min-width: 800px;
+  transform: translateX(-50%);
+}
+</style>
+
