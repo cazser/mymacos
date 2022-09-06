10c7f7e42a7e4cf701a6c34bafd91cdf591ea5fd
cazser <2264712072@qq.com>
2022-09-06 13:03:55 +0800

    引入vue-router

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import './style.css'
 import App from './App.vue'
 import Terminal from './Applications/Terminal.vue'
 
-
+import router from './router'
 
 const app = createApp(App);
-//app.use(router);
+app.use(router);
 app.mount('#app')
 
