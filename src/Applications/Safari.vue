77c103245fac1c15589c1d44414282d379eb262f
cazser <2264712072@qq.com>
2022-09-20 10:51:39 +0800

    safari first step

--- src/Applications/Safari.vue
+++ src/Applications/Safari.vue
@@ -1,18 +1,52 @@
 <script setup>
+import { onMounted, ref } from 'vue';
+
 
 defineProps({
   msg: String
 })
+const url = ref("111")
+const iframe = ref(null);
+onMounted(()=>{
+  console.log(iframe);
+})
 
+const onSubmit=()=>{
+  //console.log('提交')
+  //console.log( iframe.value);
+  iframe.value.src=url.value;
+}
 </script>
 
 <template>
-  <h1>Safari</h1>
+  <div class="wrapper">
+    <div>
+      <input  class="safariinput" v-model="url"/>
+      <button @click="onSubmit">提交</button>
+    </div>
+    <iframe ref="iframe" class="mainframe">
 
+    </iframe>
+    </div>
 </template>
 
 <style scoped>
 .read-the-docs {
   color: #888;
+}
+
+.wrapper{
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  min-height: 80vh;
+}
+
+.safariinput{
+  width: calc( 100% - 50px);
+}
+
+.mainframe{
+  flex-grow: 1;
 }
 </style>
